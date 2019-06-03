5c096267da3b1812ae5904845e87fa9ff6c2680b
Zafer <khourdaji@gmail.com>
2019-06-03 18:41:56 -0400

    use pipes, implement custom duration pipe

--- src/app/events/shared/index.js
+++ src/app/events/shared/index.js
@@ -1,13 +1,15 @@
-import {EventListResolver} from './event-list-resolver.service';
-import {EventRouteActivator} from './event-route-activator.service';
-import {IEvent, ISession} from './event.model';
-import {EventService} from './event.service';
-import {events} from './events';
+import { EventListResolver } from './event-list-resolver.service';
+import { EventRouteActivator } from './event-route-activator.service';
+import { IEvent, ISession } from './event.model';
+import { EventService } from './event.service';
+import { events } from './events';
+import { DurationPipe } from './duration.pipe';
 
 export {
   EventListResolver,
   EventRouteActivator,
   IEvent, ISession,
   EventService,
-  events
+  events,
+  DurationPipe
 }
